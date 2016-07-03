1365fde29aff270c68e80eeb723031927ac07149
Douglas Beal <git@dougbeal.com>
2016-07-03 16:22:18 -0700

    Restrict fswatch to *.m and *.h to avoid CI due to to xcode writing project files

--- ci.sh
+++ ci.sh
@@ -2,5 +2,5 @@
 this=$( cd $(dirname ${BASH_SOURCE[0]}); pwd -P )
 sg=$this/Source/Tools/ServiceGenerator/
 echo watch $sg
-fswatch --print0 -otvx --batch-marker $sg $this/run.sh --exclude .*build.* --exclude \.#.* | tee /dev/stderr | xargs -0 -n 1 -I {} bash $this/run.sh
+fswatch --print0 -otvx --batch-marker $(find $sg -name *.m -or -name *.h) $this/run.sh --exclude .*build.* --exclude \.#.* | xargs -0 -n 1 -I {} bash $this/run.sh | tee $this/Logs/ci.log
 
