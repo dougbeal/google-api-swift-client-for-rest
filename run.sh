ccdd6c4196a04629a302867a5fbba9b67041e574
Douglas Beal <git@dougbeal.com>
2016-07-07 13:12:48 -0700

    Used .json file if its there

--- run.sh
+++ run.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 this=$( cd $(dirname ${BASH_SOURCE[0]}); pwd -P )
-cd $this/Source/Tools/ServiceGenerator/
-xcodebuild install && /tmp/ServiceGenerator.dst/usr/local/bin/ServiceGenerator --outputDir $this/Output --apiLogDir $this/Logs --httpLogDir=$this/Logs  youtube:v3 --verbose --verbose
+cd "$this/Source/Tools/ServiceGenerator/"
+target="youtube:v3"
+file="$this/Logs/youtube_v3.json"
+if [ -e "$file" ]; then
+    target="$file"
+fi
+xcodebuild install && /tmp/ServiceGenerator.dst/usr/local/bin/ServiceGenerator --outputDir $this/Output --apiLogDir $this/Logs --httpLogDir=$this/Logs  "$target" --verbose --verbose
